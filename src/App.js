5667afc80e5dfc461131c099a094f35bd60d9c70
Wendell Matheus <wendellmatheus77@gmail.com>
2020-10-06 18:00:15 -0300

    component creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Header from './components/universalComponents/Header/Header'
+import HomeContent from './components/HomeContent/HomeContent'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Footer from './components/universalComponents/Footer'
+import Portfolio from './components/Portfolio/Portfolio'
+import ErrorPage from '../../components/errorPage/ErrorPage'
+
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hellow World</h1>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+        
+        <Switch>
+          <Route exact path="/">
+            <HomeContent />
+          </Route>
+
+          <Route exact path="/About">
+            <About />
+          </Route>
+
+          <Route exact path="/Contact">
+            <Contact />
+          </Route> 
+
+          <Route exact path="/Portfolio">
+            <Portfolio />
+          </Route> 
+
+          <Route >
+            <ErrorPage />
+          </Route>
+
+        </Switch> 
+       <Footer />
+      </div>
+
+    </BrowserRouter>
   );
 }
 
